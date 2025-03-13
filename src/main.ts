bbaa920b05cd8dc3d3769203b8bc8cc09a20fe66
João Vitor <jvitordiass@outlook.com>
2025-03-12 21:32:32 -0300

    style: update code formatting and linting in the project
    - update prettier config
    - add vscode settings and recommended extensions
    - update eslint config
    - remove eslint-plugin-prettier
    - update package.json scripts
    - run `npm run prettier:fix` to format the code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+const { PORT = 3000 } = process.env
+
+;(async () => {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(PORT)
+  console.log(`Server is running on http://localhost:${PORT}`)
+})().catch((err) => {
+  console.error(err)
+})
