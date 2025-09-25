19503a4cb3f652ff45da92c6d84b8d52fd6ffb0a
João Vitor <jvitordiass@outlook.com>
2025-09-24 22:13:06 -0300

    ✨ feat: set up basic nestjs application structure
    - create AppController and AppService
    - implement main application entry point
    - add unit tests for AppController
    - set up e2e tests for application
    - remove unused app.ts and utility functions

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv'
-
-import { sum } from '@/utils'
-
-dotenv.config()
-
-console.log(sum(1, 2))
-console.log(process.env.MY_SECRET_KEY)
--- src/tests/sum.test.ts
+++
@@ -1,8 +0,0 @@
-import { sum } from '@/utils'
-
-describe('Sum', () => {
-  it('Sum a and b', () => {
-    const result = sum(2, 3)
-    expect(result).toBe(5)
-  })
-})
